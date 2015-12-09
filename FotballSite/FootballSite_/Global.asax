ecb756468eee64267753fd1a59fc5640bd5e2ad1
@ruva14 <skydratm@gmail.com>
2015-12-09 16:48:45 +0300

    Ruslan. Create a new MVC Project.

---
+++ FotballSite/FootballSite_/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FootballSite_.MvcApplication" Language="C#" %>
---
+++ FotballSite/FootballSite_/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FootballSite_.Startup))]
+namespace FootballSite_
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
