c920660718c6a5b913a17051ede45ccbf76438f3
ruva14 <skydratm@gmail.com>
2015-12-10 23:59:00 +0300

    New Project

---
+++ VesSport/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VesSport.Startup))]
+namespace VesSport
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
